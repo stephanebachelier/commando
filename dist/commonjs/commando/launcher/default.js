9eb9a0c19f057561a56f137db73129a19dc03a02
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-05-27 12:15:00 +0200

    fix(launcher): use same signature with a default error callback

--- dist/commonjs/commando/launcher/default.js
+++ dist/commonjs/commando/launcher/default.js
@@ -2,21 +2,27 @@
 exports["default"] = DefaultLauncher;
 
 // Create a command launcher with `options`.
-function DefaultLauncher() {}
+function DefaultLauncher(errorHandler) {
+  this.errorHandler = errorHandler;
+}
 
 DefaultLauncher.prototype = {
   execute: function (command, args, options) {
     var errorHandler;
-    if (options && options.error && typeof options.error === 'function') {
-      errorHandler = options.error;
-    }
 
     try{
+      if (options && options.error && typeof options.error === 'function') {
+        errorHandler = options.error;
+      }
       return command.execute.apply(command, args);
     }
     catch (e) {
       if (errorHandler) {
         errorHandler(e);
+      }
+
+      if (this.errorHandler) {
+        this.errorHandler(e);
       }
     }
   }
